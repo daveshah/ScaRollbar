f80502a9722a0ca709227216f15de5aadd334e04
David Shah <david.a.shah@gmail.com>
2014-10-03 22:14:16 -0400

    Map out the trace and frames

--- src/main/scala/org/brbw/Trace.scala
+++ src/main/scala/org/brbw/Trace.scala
@@ -1,7 +1,17 @@
 package org.brbw
 
 case class Trace(t: Throwable) {
+    val frames = framesFrom(t)
+
     val exception = Exception(t.getClass.getName,t.getMessage)
+
+    private def framesFrom(t: Throwable): List[Frame] = {
+        t.getStackTrace.reverse.map( element =>
+            Frame(element.getFileName,element.getLineNumber,element.getMethodName))
+            .toList
+    }
 }
 
 case class Exception(className: String, message: String)
+
+case class Frame(filename: String, lineno: Int, method: String)
--- src/test/scala/TraceSpec.scala
+++ src/test/scala/TraceSpec.scala
@@ -4,7 +4,6 @@ import org.scalatest.{Matchers, FlatSpec}
 class TraceSpec extends FlatSpec with Matchers {
 
     it should "contain the exception" in {
-
         val testException = TestException("this is a test")
 
         val trace = Trace(testException)
@@ -18,6 +17,24 @@ class TraceSpec extends FlatSpec with Matchers {
         val trace = Trace(testException)
 
         trace.exception.message should equal ("this is a test")
+    }
+
+    it should "contain a list of frames" in {
+        val testException = TestException("this is a test")
+
+        val trace = Trace(testException)
+
+        trace.frames.length should be > 1
+    }
+
+    it should "contain the mose recent frame last in the list" in {
+        val testException = TestException("this is a test")
+
+        val frame = Trace(testException).frames.last
+
+        frame.filename should equal ("TraceSpec.scala")
+        frame.lineno should equal (31)
+        frame.method should not be null
     }
 }
 case class TestException(message: String) extends Exception(message)
