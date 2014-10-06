2833ada47c573f56c588e1c228930483ecc77c86
David Shah <david.a.shah@gmail.com>
2014-10-05 23:15:45 -0400

    JSONify the rollbar types

--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ scalaVersion := "2.11.2"
 libraryDependencies ++= Seq(
     "org.scalaj" %% "scalaj-http" % "0.3.16",
     "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test",
-    "com.typesafe.akka" %% "akka-actor" % "2.3.6"
+    "com.typesafe.akka" %% "akka-actor" % "2.3.6",
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2"
 )
 
 
--- src/main/scala/org/brbw/Model.scala
+++ src/main/scala/org/brbw/Model.scala
@@ -1,17 +1,23 @@
 package org.brbw
+import scala.util.parsing.json._
 
 case class RollbarData(accessToken: String,environment: String, throwable: Throwable) {
 
-    def asJsonString : String = {
+    private val data = Data(environment,Body(Trace(throwable)))
 
+    def asJsonString : String = toJson.toString()
 
-        ""
-    }
+    def toJson = JSONObject(Map("access_token" -> accessToken, "data" -> data.toJson))
+
 }
 
-case class Data(environment: String, body: Body)
+case class Data(environment: String, body: Body) {
+    def toJson = JSONObject(Map("environment" -> environment, "body" -> body.toJson))
+}
 
-case class Body(trace: Trace)
+case class Body(trace: Trace) {
+    def toJson =  JSONObject(Map("body" -> trace.toJson))
+}
 
 case class Trace(t: Throwable) {
     val frames = framesFrom(t)
@@ -23,8 +29,16 @@ case class Trace(t: Throwable) {
             Frame(element.getFileName,element.getLineNumber,element.getMethodName))
             .toList
     }
+
+    def toJson = {
+        JSONObject(Map("exception" -> exception.toJson,"frames" -> JSONArray(frames.map(f => f.toJson))))
+    }
 }
 
-case class Exception(className: String, message: String)
+case class Exception(className: String, message: String) {
+    def toJson =  JSONObject(Map("class" -> className, "message" -> message))
+}
 
-case class Frame(filename: String, lineno: Int, method: String)
+case class Frame(filename: String, lineno: Int, method: String) {
+    def toJson =  JSONObject(Map("filename" -> filename , "lineno" -> lineno, "method" -> method))
+}
