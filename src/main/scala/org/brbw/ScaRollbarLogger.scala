d95e19a6094433a7f10406fa32235cad236a9325
David Shah <david.a.shah@gmail.com>
2014-10-16 11:02:34 -0400

    Rename

--- src/main/scala/org/brbw/ScaRollbarLogger.scala
+++ src/main/scala/org/brbw/ScaRollbarLogger.scala
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import scalaj.http.Http
 
-case class Logger(apiKey: String, environment: String) {
+case class ScaRollbarLogger(apiKey: String, environment: String) {
 
     def log(throwable: Throwable): Throwable = {
         log(RollbarData(apiKey, environment, throwable))
@@ -17,7 +17,7 @@ case class Logger(apiKey: String, environment: String) {
     }
 
     private def log(data: RollbarData) : Unit = {
-        implicit val context = ExecutionContext.fromExecutor(Logger.pool)
+        implicit val context = ExecutionContext.fromExecutor(ScaRollbarLogger.pool)
         Future {
             Http.postData("https://api.rollbar.com/api/1/item/", data.asJsonString)
                 .header("content-type", "application/json")
@@ -26,7 +26,7 @@ case class Logger(apiKey: String, environment: String) {
     }
 }
 
-object Logger {
+object ScaRollbarLogger {
     private val pool = Executors.newCachedThreadPool()
 }
 
